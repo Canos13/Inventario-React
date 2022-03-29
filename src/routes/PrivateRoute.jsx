9e4c066dd717ca7aa735ce6e29a3fbedc68c6133
Canos13 <canomsergio13@gmail.com>
2022-03-28 21:40:20 -0600

    Nuevos componentes

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate, Route } from 'react-router-dom';
+
+export const PrivateRoute = (props) => {
+
+  
+  const user = null;
+
+  
+
+  return (
+    <Route  {...props} />
+  )
+}
